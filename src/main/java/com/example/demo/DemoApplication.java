63b191ab89e947f18bc2a07f398eb1dff1b01d81
Owen Graham <ohgraham@wisc.edu>
2023-04-06 21:28:11 -0500

    Strip out cookie demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,5 @@
 package com.example.demo;
 
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.UUID;
-
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
@@ -28,125 +18,16 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class DemoApplication {
 
-    private static OffsetDateTime startupTime;
-    private HashMap<String, UserData> userDataMap;
-    private LinkedList<String> availableRecipes;
-    private static final String[] allRecipesArray = new String[] {
-        "butter",
-        "chocolate chip",
-        "gingerbread",
-        "gingersnap",
-        "macaroon",
-        "molasses",
-        "oatmeal",
-        "oatmeal raisin",
-        "peanut butter",
-        "shortbread",
-        "snickerdoodle",
-        "sugar",
-        "white chocolate macadamia",
-    };
-
     public static void main(String[] args) {
-        startupTime = OffsetDateTime.now();
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    public DemoApplication() {
-        userDataMap = new HashMap<>();
-        List<String> allRecipes = Arrays.asList(allRecipesArray);
-        Collections.shuffle(allRecipes);
-        availableRecipes = new LinkedList<>(allRecipes);
-    }
-
     /**
-     * Display info about a user's ID and data.
+     * Say hello.
      */
     @GetMapping("/")
-    public String home(
-            @CookieValue(name="user-id", defaultValue="") String userId,
-            Model model,
-            HttpServletResponse response) {
-        UserData userData = userDataMap.get(userId);
-        boolean isNewUser = userData == null;
-        String recipe;
-        if (isNewUser) {
-            userId = UUID.randomUUID().toString();
-            userData = new UserData();
-            recipe = availableRecipes.pollFirst();
-            userData.setRecipe(recipe);
-            userDataMap.put(userId, userData);
-            response.addCookie(newCookie("user-id", userId));
-        } else {
-            recipe = userData.getRecipe();
-        }
-        model.addAttribute("isNewUser", isNewUser);
-        model.addAttribute("userId", userId);
-        model.addAttribute("recipe", recipe != null ? recipe : "nothing");
-        model.addAttribute("sorry",
-                           recipe == null || recipe.contains("raisin"));
-        model.addAttribute("startupTime", formattedTime(startupTime));
+    public String home(Model model, HttpServletResponse response) {
         return "home";
-    }
-
-    /**
-     * Set the user ID cookie to the given string and redirect home.
-     */
-    @GetMapping("/share/{userId}")
-    public RedirectView share(
-            @PathVariable String userId,
-            HttpServletResponse response) {
-        if (userDataMap.containsKey(userId)) {
-            response.addCookie(newCookie("user-id", userId));
-        }
-        return seeHome();
-    }
-
-    /**
-     * Clear cookies.
-     */
-    @GetMapping("/clear")
-    public RedirectView clear(HttpServletResponse response) {
-        response.addCookie(newCookie("user-id", "", 0));
-        return seeHome();
-    }
-
-    /**
-     * Create an HTTP 303 See Other redirect to "/".
-     */
-    private static RedirectView seeHome() {
-        RedirectView redirectView = new RedirectView();
-        redirectView.setUrl("/");
-        redirectView.setStatusCode(HttpStatus.SEE_OTHER);
-        return redirectView;
-    }
-
-    /**
-     * Create a cookie at the path "/".
-     */
-    private static Cookie newCookie(String name, String value) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/");
-        return cookie;
-    }
-
-    /**
-     * Create a cookie at the path "/" with the given expiry data.
-     */
-    private static Cookie newCookie(String name, String value, int maxAge) {
-        Cookie cookie = newCookie(name, value);
-        cookie.setMaxAge(maxAge);
-        return cookie;
-    }
-
-    /**
-     * Format date and time for display.
-     */
-    private static String formattedTime(OffsetDateTime dateTime) {
-        return dateTime
-            .withNano(0)
-            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-            .replaceFirst("T", " ");
     }
 
 }

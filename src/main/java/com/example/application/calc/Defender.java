3e3a8d596deba5acee85db1e520619277980a0bd
Owen Graham <ohgraham@wisc.edu>
2023-04-08 14:04:35 -0500

    Add damage calculation code

---
+++ src/main/java/com/example/application/calc/Defender.java
@@ -0,0 +1,44 @@
+package com.example.application.calc;
+
+public class Defender {
+    private int size;
+    private int toughness;
+    private int save;
+    private int wounds;
+    private int feelNoPain;
+    private boolean invulSave;
+
+    public Defender(int size, int toughness, int save, int wounds, int feelNoPain, boolean invulSave) {
+        this.size = size;
+        this.toughness = toughness;
+        this.save = save;
+        this.wounds = wounds;
+        this.feelNoPain = feelNoPain;
+        this.invulSave = invulSave;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public int getToughness() {
+        return this.toughness;
+    }
+
+    public int getSave() {
+        return this.save;
+    }
+
+    public int getWounds() {
+        return this.wounds;
+    }
+
+    public int getFeelNoPain() {
+        return this.feelNoPain;
+    }
+
+    public boolean getInvulSave() {
+        return this.invulSave;
+    }
+}
+

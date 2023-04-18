c5c0f22585c9ce0bd3f510c21d1d336efef20d93
Kenneth Ring <kennethgring@gmail.com>
2023-04-18 13:47:44 -0500

    Basic skeleton

---
+++ Bridge.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public interface Bridge {
+    Entry<Attacker> saveAttacker(UserId userId, Attacker attacker);
+    Entry<Weapon> saveWeapon(UserId userId, Weapon weapon);
+    Entry<Defender> saveDefender(UserId userId, Defender defender);
+    boolean saveSimulation(UserId userId, Pk attackerPk, Pk weaponPk, Pk defenderPk, Modifiers modifiers, FixedSimResults results);
+    List<Entry<Attacker>> loadAttackers(UserId userId);
+    List<Entry<Weapon>> loadWeapons(UserId userId);
+    List<Entry<Defender>> loadDefenders(UserId userId);
+    List<Simulation> loadSimulations(UserId userId);
+}
+
+class UserId {
+    // Fields and methods for UserId class
+}
+
+class Pk {
+    // Fields and methods for Pk class
+}
+
+class Simulation {
+    // Fields and methods for Simulation class
+}
+
+class FixedSimResults {
+    // Fields and methods for FixedSimResults class
+}
+
+class Entry<UnitType> {
+    // Fields and methods for Entry class
+}
+
+class Attacker {
+    // Fields and methods for Attacker class
+}
+
+class Weapon {
+    // Fields and methods for Weapon class
+}
+
+class Defender {
+    // Fields and methods for Defender class
+}
+
+class Modifiers {
+    // Fields and methods for Modifiers class
+}
+
+class UnitType {
+    // Fields and methods for UnitType class
+    // This class can be extended by Attacker, Weapon, and Defender classes
+}
+
+

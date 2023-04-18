1a475ab06c6c5e31ec87cc0202eefc67485b5d07
Patrick Merchant <pmerchant@royal-14.cs.wisc.edu>
2023-04-18 12:08:42 -0500

    application skeleton request params

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -96,7 +96,23 @@ public class Application {
      * Add a new weapon.
      */
     @PostMapping("/new-weapon")
-    public String NewWeapon() {
+    public String NewWeapon(@RequestParam(name="wep-name") String wepName,
+                            @RequestParam(name="wep-number") int wepNumber,
+                            @RequestParam(name="wep-type") String wepType,
+                            @RequestParam(name="wep-attacks") int wepAttacks,
+                            @RequestParam(name="wep-strength") int wepStrength,
+                            @RequestParam(name="wep-armor-pen") int wepArmorPen,
+                            @RequestParam(name="wep-damage") int wepDamage,
+                            @RequestParam(name="wep-exploding-hits", defaultValue="off")
+                boolean wepExplodingHits,
+                            @RequestParam(name="wep-mort-wound-six-hit", defaultValue="off")
+                boolean wepMortWoundSixHit,
+                            @RequestParam(name="wep-mort-wound-six-wound", defaultValue="off")
+                boolean wepMortWoundSixWound,
+                            @RequestParam(name="wep-add-ap-six-wound", defaultValue="off")
+                boolean wepAddApSixWound,
+                            Model model,
+                            HttpServletResponse response) {
         System.out.println("created new weapon");
         return "home";
     }
@@ -105,7 +121,30 @@ public class Application {
      * Add a new attacker.
      */
     @PostMapping("/new-attacker")
-    public String NewAttacker() {
+    public String NewAttacker(@RequestParam(name="atk-name") String atkName,
+                              @RequestParam(name="atk-bal-skill") int atkBalSkill,
+                              @RequestParam(name="atk-wep-skill") int atkWepSkill,
+                              @RequestParam(name="atk-wep-one") String atkWepOne,
+                              @RequestParam(name="atk-wep-two") String atkWepTwo,
+                              @RequestParam(name="atk-wep-three") String atkWepThree,
+                              @RequestParam(name="atk-plus-one-to-hit", defaultValue="off")
+                boolean atkPlusOneToHit,
+                              @RequestParam(name="atk-minus-one-to-hit", defaultValue="off")
+                boolean atkMinusOneToHit,
+                              @RequestParam(name="atk-plus-one-to-wound", defaultValue="off")
+                boolean atkPlusOneToWound,
+                              @RequestParam(name="atk-minus-one-to-wound", defaultValue="off")
+                boolean atkMinusOneToWound,
+                              @RequestParam(name="atk-reroll-hits", defaultValue="off")
+                boolean atkRerollHits,
+                              @RequestParam(name="atk-reroll-hits-roll-of-one", defaultValue="off")
+                boolean atkRerollHitsRollOfOne,
+                              @RequestParam(name="atk-reroll-wounds", defaultValue="off")
+                boolean atkRerollWounds,
+                              @RequestParam(name="atk-reroll-wounds-roll-of-one", defaultValue="off")
+                boolean atkRerollWoundsRollOfOne,
+                              Model model,
+                              HttpServletResponse response) {
         System.out.println("created new attacker");
         return "home";
     }
@@ -114,7 +153,26 @@ public class Application {
      * Add a new attacker.
      */
     @PostMapping("/new-defender")
-    public String NewDefender() {
+    public String NewDefender(@RequestParam(name="def-name") String defName,
+                              @RequestParam(name="def-size") int defSize,
+                              @RequestParam(name="def-toughness") int defToughness,
+                              @RequestParam(name="def-save") int defSave,
+                              @RequestParam(name="def-wounds") int defWounds,
+                              @RequestParam(name="def-fnp") int defFnp,
+                              @RequestParam(name="def-plus-one-to-save", defaultValue="off")
+                boolean defPlusOneToSave,
+                              @RequestParam(name="def-minus-one-to-save", defaultValue="off")
+                boolean defMinusOneToSave,
+                              @RequestParam(name="def-invul-save", defaultValue="off")
+                boolean defInvulSave,
+                              @RequestParam(name="def-reroll-saves", defaultValue="off")
+                boolean defRerollSaves,
+                              @RequestParam(name="def-reroll-saves-of-one", defaultValue="off")
+                boolean defRerollSavesOfOne,
+                              @RequestParam(name="def-minus-one-to-damage", defaultValue="off")
+                boolean defMinusOneToDamage,
+                              Model model,
+                              HttpServletResponse response) {) {
         System.out.println("created new defender");
         return "home";
     }
@@ -123,7 +181,10 @@ public class Application {
      * Add a new simulation.
      */
     @PostMapping("/new-simulation")
-    public String NewSimulation() {
+    public String NewSimulation(@RequestParam(name="atk-name") String atkName,
+                                @RequestParam(name="def-name") String defName,
+                                Model model,
+                                HttpServletResponse response) {
         System.out.println("created new simulation");
         return "simulation";
     }   
